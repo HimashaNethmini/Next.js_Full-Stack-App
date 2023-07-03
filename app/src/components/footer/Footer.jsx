9d4f53f806831f5a10971dea5e02d685bf4536f8
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-03 20:01:39 +0530

    Updated final footer

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import styles from "./footer.module.css";
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div>
-      <div>2023 Himasha. All rights reserved. </div>
-      <div></div>
+    <div className={styles.container}>
+      <div>©2023 Lamamia. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt=" Facebook Account" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="instagram" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="twitter" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Youtube" />
+      </div>
     </div>
-  )
+  );
 };
 
 export default Footer;
