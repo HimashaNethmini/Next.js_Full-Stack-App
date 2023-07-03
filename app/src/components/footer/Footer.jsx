aa20f7e51e4dc0883c5fe8d568948568c6c2867c
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-03 10:03:10 +0530

    Create footer

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      Hi im footer........
+      <div>2023 Himasha. All rights reserved. </div>
+      <div></div>
     </div>
   )
-}
+};
 
 export default Footer;
