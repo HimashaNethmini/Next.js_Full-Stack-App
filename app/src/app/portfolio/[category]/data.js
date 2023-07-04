26f89ba0b69d16ca9fe62950e8f7d75fad377a97
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-04 12:45:58 +0530

    All the data in portfolio pages

---
+++ app/src/app/portfolio/[category]/data.js
@@ -0,0 +1,70 @@
+export const items = {
+    applications: [
+      {
+        id: 1,
+        title: "Creative Portfolio",
+        desc: "Creativity is a dance of imagination and expression, where dreams take flight and boundaries fade away. Through art, photography, and design, we explore the boundless possibilities of human creativity, seeking inspiration in the intricate patterns of the world around us.",
+        image:  "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      },
+      {
+        id: 2,
+        title: "Minimal Single Product",
+        desc: "Join us on this artistic odyssey as we celebrate the power of human imagination and unleash the beauty that resides within each of us",
+        image:
+          "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      },
+      {
+        id: 3,
+        title: "Strong Together Charity",
+        desc: "Nature is an artist, and our planet is its breathtaking masterpiece.From the majestic mountains to the tranquil shores, from the vibrant hues of a sunset to the delicate petals of a flower, every detail is a stroke of genius. ",
+        image:
+          "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+      }
+    ],
+    Illustrations: [
+      {
+        id: 1,
+        title: "Creative Portfolio",
+        desc: "Join us as we embark on a visual journey, capturing the wonders of our natural world, and reminding ourselves of the profound beauty that lies just beyond our doorstep",
+        image:
+          "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      },
+      {
+        id: 2,
+        title: "Minimal Single Product",
+        desc: "In a world that often rushes by in a blur, we invite you to pause, take a deep breath, and immerse yourself in the beauty of the present moment. Let your senses awaken as you explore our collection of exquisite photographs, captivating stories, and thought-provoking art. ",
+        image:
+          "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      },
+      {
+        id: 3,
+        title: "Strong Together Charity",
+        desc: "Together, let's create a sanctuary of beauty and ignite the spark of inspiration within",
+        image:
+          "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+      }
+    ],
+    websites: [
+      {
+        id: 1,
+        title: "Creative Portfolio",
+        desc: "Welcome to our little corner of the internet, where beauty and inspiration collide.",
+        image:
+          "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+      },
+      {
+        id: 2,
+        title: "Minimal Single Product",
+        desc: "Here, we believe that life is a canvas, and each day is an opportunity to paint it with vibrant colors, breathtaking moments, and unforgettable experiences. ",
+        image:
+          "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+      },
+      {
+        id: 3,
+        title: "Strong Together Charity",
+        desc: "Join us on this journey as we celebrate the extraordinary in the ordinary and discover the beauty that surrounds us",
+        image:
+          "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+      }
+    ],
+  };
