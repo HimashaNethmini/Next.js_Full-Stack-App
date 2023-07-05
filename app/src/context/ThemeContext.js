828d7235eea01eb3b308a1cc83cc281953e59876
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-05 19:30:13 +0530

    change colour toggle

---
+++ app/src/context/ThemeContext.js
@@ -0,0 +1,23 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({children})=> {
+    const [mode, setMode] = useState("dark");
+
+    const toggle = () => {
+        setMode((prev) => (prev === "dark" ? "light" : "dark"));
+    };
+
+    return (
+        //sending any value to children
+        <ThemeContext.Provider value={{toggle, mode}} >
+           <div className={`theme ${mode}`}>
+            {children}
+           </div>
+
+        </ThemeContext.Provider>
+    )
+}
