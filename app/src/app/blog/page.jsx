2a2085b2f4530e58a58074718068000330f2e74f
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-08 11:59:07 +0530

    Create the blog page

--- app/src/app/blog/page.jsx
+++ app/src/app/blog/page.jsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", 
+  const res = await fetch("http://localhost:3000/api/posts",
   {
     cache: "no-store",
-  });
+  }); 
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
